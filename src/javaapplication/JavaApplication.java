874200e9937a698583a1df0c0dfe488a5b0e681a
Rishabh Dev <rishabhdev190@gmail.com>
2026-08-15 17:14:26 +0530

    Sync BillingApplication structure for NetBeans

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -2,19 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.restaurant;
+package javaapplication;
 
 /**
  *
- * @author risha
+ * @author Manvi Walam
  */
-public class Main {
+public class JavaApplication{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+ public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(() -> {
+        new NewJFrame().setVisible(true);
+    });
+}
     
 }
